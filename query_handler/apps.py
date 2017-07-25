99fa16f0429bea7f2086ca9f27a0f8777011ff02
Woojgh <jamessalamonsen@gmail.com>
2017-07-25 10:45:08 -0700

    requirements

--- query_handler/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QueryHandlerConfig(AppConfig):
-    name = 'query_handler'
--- query_profile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from query_profile.views import ProfileView, EditProfileView
-
-app_name = 'query_profile'
-
-urlpatterns = [
-    url(r'^$', ProfileView.as_view(), name='profile'),
-    # url(r'^edit/$', EditProfileView.as_view(), name='edit_profile'),
-]
