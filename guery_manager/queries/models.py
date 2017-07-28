2bd7590f2a7a0be43925b3618aa5558a44bdde93
James <jjfeore>
2017-07-27 21:16:23 -0700

    Tweaked the queries model

--- guery_manager/queries/models.py
+++ guery_manager/queries/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Queries(models.Model):
     name = models.CharField(max_length=100)
     project = models.CharField(max_length=255, null=True)
-    query_text = models.CharField(max_length=255)
-    schedule = models.CharField(max_length=255)
+    query_text = models.CharField(max_length=4096)
+    schedule = models.CharField(max_length=100)
     last_run = models.CharField(null=True, blank=True, max_length=100)
     run_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='queries')
 
@@ -19,7 +19,7 @@ class QueryInstance(models.Model):
     root_url = models.CharField(max_length=255, null=True)
     visual_url = models.URLField(max_length=255, null=True)
     queries = models.ForeignKey(Queries, on_delete=models.CASCADE, related_name='instances')
-    status = models.CharField(default='Pending', max_length=255)
+    status = models.CharField(default='Pending', max_length=100)
 
     def __repr__(self):
         """."""
